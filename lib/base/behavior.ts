b4a9933fbee8ef4062f544567d6a5eeada64cfdf
TbhT <hustapple132@gmail.com>
2019-07-02 21:57:22 +0800

    feat(dev): add ts files

---
+++ lib/base/behavior.ts
@@ -0,0 +1,40 @@
+import Component from './component'
+
+interface $Events {
+  [key: string]: any
+}
+
+export default class Behavior {
+  public owner: Component | null
+
+  constructor() {
+    this.owner = null
+  }
+
+  events(): $Events {
+    return {}
+  }
+
+  attach(owner: Component): void {
+    this.owner = owner
+    this.tach('on')
+  }
+
+  detach(): void {
+    if (this.owner) {
+      this.tach('off')
+      this.owner = null
+    }
+  }
+
+  private tach(type: 'on' | 'off') {
+    const events: $Events = this.events()
+
+    for (const event in events) {
+      if (events.hasOwnProperty(event)) {
+        const fn: Function = (<Component>this.owner)[type]
+        fn(event, events[event])
+      }
+    }
+  }
+}
---
+++ lib/base/component.ts
@@ -0,0 +1,43 @@
+import { EventEmitter } from 'events'
+import Behavior from './behavior'
+
+interface Behaviors {
+  [key: string]: Behavior
+}
+
+export default class Component extends EventEmitter {
+  private _behaviors: Behaviors | null
+
+  constructor() {
+    super()
+    this._behaviors = null
+  }
+
+  private attachBehaviorInternal(name: string, behavior: Behavior) {
+    if ((<Behaviors>this._behaviors)[name]) {
+      ;(<Behaviors>this._behaviors)[name].detach()
+    }
+
+    behavior.attach(this)
+    ;(<Behaviors>this._behaviors)[name] = behavior
+  }
+
+  private attachBehaviorsInternal(behaviors: Behaviors) {
+    for (const behavior in behaviors) {
+      if (behaviors.hasOwnProperty(behavior)) {
+        this.attachBehaviorInternal(name, behaviors[behavior])
+      }
+    }
+  }
+
+  behaviors(): Behaviors {
+    return {}
+  }
+
+  attachBehaviors() {
+    if (this._behaviors === null) {
+      this._behaviors = {}
+      this.attachBehaviorsInternal(this.behaviors())
+    }
+  }
+}

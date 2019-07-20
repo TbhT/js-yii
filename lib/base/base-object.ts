0807ac5d234bec2fe6e98ab49d24a24a90e7acd0
TbhT <hustapple132@gmail.com>
2019-07-21 00:21:03 +0800

    feat(base): add base object class

---
+++ lib/base/base-object.ts
@@ -0,0 +1,45 @@
+import { Configurable, ConfigureObj } from './configurable'
+
+export interface ProxyObj {
+  [key: string]: any
+}
+
+export abstract class BaseObject implements Configurable {
+  [key: string]: any
+
+  constructor(config: ConfigureObj = {}) {
+    if (Object.keys(config).length > 0) {
+      for (const key in config) {
+        if (config.hasOwnProperty(key)) {
+          this.proxy[key] = config[key]
+        }
+      }
+    }
+
+    this.init()
+  }
+
+  abstract init(): void
+
+  canGetProperty(name: string): boolean {
+    return !!(
+      this[`get${name}`] ||
+      (this[name] && typeof this[name] !== 'function')
+    )
+  }
+
+  canSetProperty(name: string): boolean {
+    return !!(
+      this[`set${name}`] ||
+      (this[name] && typeof this[name] !== 'function')
+    )
+  }
+
+  hasProperty(name: string): boolean {
+    return this.canGetProperty(name) || this.canSetProperty(name)
+  }
+
+  hasMethod(name: string): boolean {
+    return !!(this[name] && typeof this[name] === 'function')
+  }
+}

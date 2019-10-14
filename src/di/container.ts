8eed2c24d0943d773562085cd7617c82ac1822f0
TbhT <hustapple132@gmail.com>
2019-10-14 11:59:37 +0800

    :construction: update container

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,3 +1,8 @@
+import { IndexableObj, Definition } from '../factory/definitions'
+import { ServiceProviderInterface, DeferredServiceProvider } from './contracts'
+import { isString } from 'util'
+import { CircularReferenceError } from '../factory/exceptions'
+
 /**
  * base interface representing a generic Error in a container
  */
@@ -6,8 +11,7 @@ export interface ContainerErrorInterface {}
 /**
  * no entry was found in the container
  */
-export interface NotFoundErrorInterface
-  extends ContainerErrorInterface {}
+export interface NotFoundErrorInterface extends ContainerErrorInterface {}
 
 export interface ContainerInterface {
   /**
@@ -29,20 +33,70 @@ export interface ContainerInterface {
   has(id: string): boolean
 }
 
-
 export class Container implements ContainerInterface {
-  private definitions = new Map()
+  private definitions: Map<any, Definition> = new Map()
 
   // used to collect ids instantiated during build to detect circular references
   private building = new Map()
 
   // list of providers deferred to register till their service would be requested
-  private deferredProviders = new Map()
+  private deferredProviders: Map<any, DeferredServiceProvider> = new Map()
 
   private instances: Map<any, object> = new Map()
 
-  
+  constructor(
+    definitions: IndexableObj = {},
+    providers: ServiceProviderInterface[] = []
+  ) {
+    // todo:
+  }
 
+  public getId(id: any): string {
+    return isString(id) ? id : id.getId()
+  }
 
+  public get(id: string, params: any[] = []): object {
+    id = this.getId(id)
+    if (this.instances.has(id) === false) {
+      this.instances.set(id, this.build(id, params))
+    }
 
+    return <object>this.instances.get(id)
+  }
+
+  public build(id: string, params: any[] = []): object {
+    if (this.building.has(id)) {
+      throw new CircularReferenceError(
+        `Circular reference to ${id} detected while building: ${this.building}`
+      )
+    }
+
+    this.building.set(id, 1)
+    this.registerProviderIfDeferredFor(id)
+  }
+
+  public has(id: string): boolean {
+    return this.definitions.has(id)
+  }
+
+  private buildInternal(id: string, params: any[] = []) {
+    if (this.definitions.has(id) === false) {
+      return this.buildPrimitive(id, params)
+    }
+
+    return (<Definition>this.definitions.get(id)).resolve(this, params)
+  }
+
+  private registerProviderIfDeferredFor(id: string) {
+    const providers = this.deferredProviders
+
+    for (const [key, provider] of providers.entries()) {
+      if (provider.hasDefinitionFor(id)) {
+        provider.register(this)
+
+        // provider should be removed after registration to not be registered again
+        providers.delete(key)
+      }
+    }
+  }
 }
--- src/di/contracts.ts
+++ src/di/contracts.ts
@@ -1,9 +1,93 @@
 import { Container } from './container'
+import { IndexableObj } from '../factory/definitions'
 
-interface ServiceProviderInterface {
+/**
+ * represents a component responsible for class registration in the Container.
+ *
+ * ```javascript
+ *
+ * class CarProvider implements ServiceProviderInterface {
+ *   protected container
+ *
+ *   constructor(container) {
+ *     this.container = container
+ *   }
+ *
+ *   public register() {
+ *     this.registerDependencies()
+ *     this.registerService()
+ *   }
+ *
+ *   protected registerDependencies() {
+ *     const container = this.container
+ *     container.set(Engine.name, Engine)
+ *     container.set(Wheel.name, {
+ *        className: Wheel,
+ *        color: 'red'
+ *     })
+ *   }
+ *
+ *   protected registerService() {
+ *     this.container.set(Car.name, {
+ *        className: Car,
+ *        color: 'red'
+ *     })
+ *   }
+ *
+ * }
+ *
+ * ```
+ */
+export interface ServiceProviderInterface {
   register(container: Container): void
+}
+
+export abstract class ServiceProvider implements ServiceProviderInterface {
+  abstract register(container: Container): void
 }
 
 interface DeferredServiceProviderInterface extends ServiceProviderInterface {
   hasDefinitionFor(id: string): boolean
+}
+
+/**
+ * Base class for service providers that should be deferred to register till services are
+ * actually required.
+ *
+ * Complex services with heavy dependencies might create redundant load during bootstrapping
+ * of an application so to reduce actions performed during the container bootstrap you can
+ *
+ * Deferred providers can be added to the Container like basic providers but won't register
+ * any definitions to the container till one of the classes listed in `provides` method would
+ * be requested from container. Example:
+ *
+ * ```javascript
+ *
+ * class CarProvider extends DeferredServiceProvider {
+ *    public provides() {
+ *      return {
+ *        car: Car,
+ *        carFactory: CarFactory
+ *      }
+ *    }
+ *
+ *    public register() {
+ *      const container = this.container
+ *      container.set('car', Car)
+ *      container.set('carFactory', CarFactory)
+ *      container.set('engine', EngineMarkOne)
+ *    }
+ * }
+ *
+ * ```
+ */
+export abstract class DeferredServiceProvider
+  implements DeferredServiceProviderInterface {
+  public abstract register(container: Container): void
+
+  public abstract provides(): IndexableObj
+
+  public hasDefinitionFor(id: string): boolean {
+    return id in this.provides()
+  }
 }
